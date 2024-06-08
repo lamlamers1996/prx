7517ca00997f881835d6d5b67e55bc3ee7816d68
lamlamers1996 <69657589+lamlamers1996@users.noreply.github.com>
2024-06-08 20:32:41 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -14,24 +14,28 @@ port=6789
 user="admin"  # Thay bằng username của bạn
 pass="adminbang123"  # Thay bằng password của bạn
 
+# Danh sách proxy lỗi
+declare -a failed_proxies
+
+# Kiểm tra từng proxy trong tệp
 while IFS= read -r proxy; do
     # Sử dụng curl để kiểm tra proxy
-    response=$(curl --socks5 "$user:$pass@$proxy:$port" -s -o /dev/null -w "%{http_code}" http://www.google.com)
+    response=$(curl --socks5 "$user:$pass@$proxy:$port" --max-time 10 -s -o /dev/null -w "%{http_code}" http://www.google.com)
 
     if [ "$response" -eq 200 ]; then
-        echo "Proxy $proxy:$port proxy hoạt động"
+        echo "Proxy $proxy:$port hoạt động"
     else
-        echo "Proxy $proxy:$port proxy lỗi"
+        echo "Proxy $proxy:$port Lỗi."
         failed_proxies+=("$proxy:$port")
     fi
 done < "$proxy_file"
 
 # In ra các proxy lỗi
 if [ ${#failed_proxies[@]} -ne 0 ]; then
-    echo "The following proxies are not working:"
+    echo "danh sách proxy lỗi:"
     for proxy in "${failed_proxies[@]}"; do
         echo "$proxy"
     done
 else
-    echo "All proxies are working."
+    echo "tất cả proxy hoạt động"
 fi
